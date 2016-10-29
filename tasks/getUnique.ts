1208a0d373f2e63ff8607c44b968a5dcc48b4154
aleksandr <alx.panteleev@gmail.com>
2016-10-29 10:56:04 +0300

    adding generic <T> types

--- tasks/getUnique.ts
+++ tasks/getUnique.ts
@@ -1,7 +1,9 @@
 'use strict'
 
-function getUnique (...args: number[]): number[] {
-    let result: number[] = []
+type jsPrimitives = string | number | boolean
+
+function getUnique<T> (...args: T[]): T[] {
+    let result: T[] = []
     for (let i=0; i < args.length; i++) {
         if (result.indexOf(args[i]) === -1) {
             result.push(args[i])
@@ -10,3 +12,6 @@ function getUnique (...args: number[]): number[] {
 
     return result
 }
+
+const uniqArray = getUnique<jsPrimitives>(1,2,'cow',1,false, 6,true, 'cow', false, false, 2)
+console.log(uniqArray)
